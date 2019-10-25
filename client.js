74f9e4d600856b1d6528e668192431a3471ecc77
James Ward <james.parker.ward@gmail.com>
2019-10-25 17:13:04 +0100

    Now rendering messages in chat box

--- client.js
+++ client.js
@@ -1,27 +1,51 @@
 const textInput = document.getElementById('chat-input');
 const sendBtn = document.getElementById('btn-send');
+const chatList = document.querySelector('.chat-list');
 const ws = new WebSocket('ws://localhost:1017');
+
+//Get username
+const username = prompt('What is your username?');
+const userObj = {type: "user", username:username, colour:"green"};
 
 //Websocket connected
 ws.onopen = () => {
-    console.log('Connection to websocket open...');
+    console.log(`Connected to chat as ${username}...`);
+    ws.send(JSON.stringify(userObj));
 };
 
 //Update chat
-ws.onmessage = (e) => {
-    console.log(`${e.data}`);
+ws.onmessage = (message) => {
+
+    var message = JSON.parse(message.data);
+
+    switch(message.type){
+        case "message":
+            console.log(`${message.username}:${message.message}`);
+
+            //Create chat item
+            var msgElement = document.createElement('li');
+            msgElement.innerHTML = `<p class="message-username">${message.username}</p><p class="message-text">${message.message}</p>`;
+
+            chatList.appendChild(msgElement);
+
+            break;
+        case "user":
+            console.log(`${message.username}`);
+            break;
+    }
+
 };
 
 //Client leaves
-ws.onclose = () => {
-    console.log("Disconnected");
-};
+// ws.onclose = () => {
+//     console.log("Disconnected");
+//     var dcObject = {type: "disconnect", username:username};
+//     ws.send(JSON.stringify(dcObject));
+// };
 
 //When send is clicked
 sendBtn.addEventListener('click', function(e){
-
     //Get text from input field
-    var msg = textInput.value;
-    ws.send(msg);
-
+    var messageObject = {type: "message", username:username, message:textInput.value};
+    ws.send(JSON.stringify(messageObject));
 });
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+//Modules
 const WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({port: 1017}),
     http = require('http'),
     httpPort = 1337;
+
+//Chat variables
+var messages = [];
+var activeUsers = [];
 
 http.createServer((req, res) => {
 
@@ -13,9 +18,40 @@ http.createServer((req, res) => {
 
 wss.on('connection', (ws) => {
 
+    //Add to active users
+    console.log('Running');
+
     //Message received
     ws.on('message', (message) => {
-        console.log(message);
+
+        //Parse message to JSON obj
+        message = JSON.parse(message);
+
+        //Process message
+        switch (message.type) {
+            case "user":
+                //If object is type user add to client list
+                activeUsers.push(message);
+                break;
+            case "message":
+                //Log message to console
+                sendToChat(message);
+                console.log(`Message received from ${message.username}: ${message.message}`);
+                break;
+        }
     });
+
+    ws.on('close', (connection) => {
+        //Remove client from list
+        console.log('Client disconnected');
+    });
+
+    function sendToChat(message){
+        wss.clients.forEach((client) => {
+            if(client.readyState == ws.OPEN){
+                client.send(JSON.stringify(message));
+            }
+        });
+    }
 
 });
